d3e2c4ae7178a753347719228f7861b192268533
theo gandara <theogandara@gmail.com>
2021-08-25 00:05:55 -0300

    Realizado o extra

--- tabela.js
+++ tabela.js
@@ -1,4 +1,22 @@
-function exibeTabela(n) { 
+function exibeTabela() { 
+    
+    const tabela = [];
+
+    for (let i = 0; i <= 10; i++) {
+
+        const linha = [];
+
+        for (let o = 0; o <= 10; o++){
+
+            linha.push(o * i);
+
+        }
+    tabela.push(linha)
+    }
+    console.table(tabela)
+}
+
+function exibeTabelaExtra(n) { 
     
     const tabela = [];
 
